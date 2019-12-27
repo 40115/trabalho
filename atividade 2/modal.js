9e858bed5609a921c7739fc3ccb149ecceaf89f2
CarameloxD <57467882+CarameloxD@users.noreply.github.com>
2019-12-27 22:45:33 +0000

    Add files via upload

---
+++ atividade 2/modal.js
@@ -0,0 +1,40 @@
+// Get DOM Elements
+const modal = document.querySelector('#my-modal');
+//const modal = $("#my-modal")
+const modalBtn = document.querySelector('#modal-btn');
+const closeBtnX = document.querySelector('.close');
+const closeBtn = document.querySelector('#buttonClose');
+
+
+// Events
+modalBtn.addEventListener('click', openModal);
+closeBtnX.addEventListener('click', closeModal);
+closeBtn.addEventListener('click', closeModal);
+window.addEventListener('click', outsideClick);
+
+
+// Open
+function openModal() {
+  modal.style.display = 'block';
+}
+
+// Close
+function closeModal() {
+  modal.style.display = 'none';
+}
+
+
+// Close If Outside Click
+function outsideClick(e) {
+  if (e.target == modal) {
+    modal.style.display = 'none';
+  }
+}
+
+//Calculate
+function calculate() {
+  var op1 = parseInt(document.getElementById("operand1").value)
+  var op2 = parseInt(document.getElementById("operand2").value)
+  alert (document.getElementById("result").value = op1 * op2)
+}
+
